0426a01ec55e827fb63c25050674d28078b82c34
anthonyalando8 <103507949+anthonyalando8@users.noreply.github.com>
2022-12-02 13:59:56 +0300

    Update Login_js.js

--- Login_js.js
+++ Login_js.js
@@ -1,27 +1,71 @@
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain attribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" href="favicon.ico">
+<link rel="stylesheet" href="Login_css.css">
+<link rel="stylesheet" href="page_layout_default.css">
+<script src="Login_js.js"></script>
+<script src="https://www.googleoptimize.com/optimize.js?id=OPT-WQWZ5ZM"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://kit.fontawesome.com/3695de9773.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="navbar">
+    <a href="index.html"><i class="fa fa-fw fa-home"></i> Home</a>
+    <a href="Menu.html"><i class="fa-solid fa-cake-candles"></i> Products</a>
+    <a href="orders.html"><i class="fa-solid fa-truck-fast"></i>Orders</a>
+    <a href="Reservations.html"><i class="fa-solid fa-table"></i> Reservation</a>
+    <a href="shops.html"><i class="fa-solid fa-shop"></i> Shops</a>
+    <a href="about.html"><i class="fa-solid fa-address-card"></i> About Us</a>
+    <a href="Gallery.html"><i class="fa-solid fa-image"></i> Gallery</a>
+    <a href="account.html"><i class="fa-solid fa-circle-info"></i> Account</a>
+    <a class="active"href="login.html"><i class="fa fa-fw fa-user"></i> Login</a>
+    <a href=""><i class="fa fa-fw fa-search"></i> Search</a>
+</div>
+  <h2>Modal Login Form</h2>
+
+  <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
+  
+  <div id="id01" class="modal">
+    
+    <form class="modal-content animate" action="/action_page.php" method="post">
+      <div class="imgcontainer">
+        <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+        <img src="https://ik.imagekit.io/anthonyalando/Images/user.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1669121593852" alt="Avatar" class="avatar">
+      </div>
+  
+      <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+  
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+          
+        <button type="submit">Login</button>
+        <label>
+          <input type="checkbox" checked="checked" name="remember"> Remember me
+        </label>
+      </div>
+  
+      <div class="container" style="background-color:#f1f1f1">
+        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+        <span class="psw">Forgot <a href="#">password?</a></span>
+      </div>
+    </form>
+  </div>
+  
+  <script>
+  // Get the modal
+  var modal = document.getElementById('id01');
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
   }
-};
+  </script>
+
+</body>
+</html>
